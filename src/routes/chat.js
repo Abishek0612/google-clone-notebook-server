1a25db53489c316405bd767e890e3aed5a67ae2e
Abishek0612 <uabishek6@gmail.com>
2025-07-29 14:10:11 +0530

    bug fix in chat.js

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -3,20 +3,11 @@ const chatController = require("../controllers/chatController");
 
 const router = express.Router();
 
-router.post("/chat/message", chatController.sendMessage);
-router.get("/chat/conversation/:pdfId", chatController.getConversation);
-router.delete(
-  "/chat/conversation/:pdfId/:messageId",
-  chatController.deleteMessage
-);
-router.delete(
-  "/chat/conversation/:pdfId/clear",
-  chatController.clearConversation
-);
-
 router.post("/message", chatController.sendMessage);
 router.get("/conversation/:pdfId", chatController.getConversation);
 router.delete("/conversation/:pdfId/:messageId", chatController.deleteMessage);
 router.delete("/conversation/:pdfId/clear", chatController.clearConversation);
+
+router.post("/search-similar", chatController.searchSimilar);
 
 module.exports = router;
